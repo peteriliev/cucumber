cf4722402d50e1fd4ff17c0392a093f53acdd1a4
Peter.Iliev@javelingroup.com <peteriliev@gmail.com>
2014-05-06 16:45:17 +0300

    Added raw full registration

--- features/first.feature
+++ features/first.feature
@@ -1,6 +1,5 @@
 Feature: Sanity check
 
-  @wip
   Scenario: Search for "antibodies"
 	Given I navigate to the US channel Home Page
     When I search for "antibodies"
@@ -17,3 +16,38 @@ Feature: Sanity check
 	And I enter to Quick Purchase field 4 SKU MM_NF-SLGSV255F
 	And I navigate to Shopping Cart page
     Then I see a subtotal of USD 185.43
+
+  Scenario: Verify mega nav
+    Given I navigate to the US channel Home Page
+	When I open the Mega-Nav "Products" menu
+	And I select "Biopharmaceutical Manufacturing" sub-menu
+	And I select "Harvest" sub-sub-menu
+	Then I get a CMS page containing text "we offer a range of solutions that optimize quality"
+	And I get a CMS page containing left menu "Downstream Processing"
+	And I get a top left CMS area containing "Interactive Demo"
+	
+  @wip
+  Scenario: Registration
+    Given I navigate to the US channel Home Page
+	When I navigate to the register page
+	And I select Full Registration
+	And I fill-in these fields at Register page:
+		| Field									| Value						|
+		| MerckFullRegistration_Login			| peteriliev7@hotmail.com	|
+		| MerckFullRegistration_Password		| pass1234					|
+		| MerckFullRegistration_RetypedPassword	| pass1234					|
+		| MerckFullRegistration_Title			| Mr.						|
+		| MerckFullRegistration_FirstName		| Peter						|
+		| MerckFullRegistration_LastName		| Iliev						|
+		| MerckFullRegistration_Company			| Acme Motors				|
+		| MerckFullRegistration_Telephone		| +359888916361				|	
+	And I fill-in these fields at Address page:
+		| Field												| Value						|
+		| MerckAddressFormWithState_ShippingAddressLineOne	| 20 Main Str				|
+		| MerckAddressFormWithState_ShippingAddressLineTwo	| 							|
+		| MerckAddressFormWithState_ShippingAddressLineThree| 							|
+		| MerckAddressFormWithState_ShippingCity			| Ocean City				|
+		| MerckAddressFormWithState_ShippingPostalCode		| 4000						|
+		| MerckAddressFormWithState_ShippingCountryCode		| United States				|
+		| MerckAddressFormWithState_ShippingState			| New Jersey				|
+	Then I get a page reading "Thank You for Registering" and "Activation Code"
--- features/step_definitions/first-steps.rb
+++ features/step_definitions/first-steps.rb
@@ -21,7 +21,7 @@ When(/^I search for "(.*?)"$/) do |term|
 end
 
 Then(/^I get (\d+) ([^ ]*) as a search result$/) do |count, type|
-	page.should have_content type + ' (' + number_with_delimiter(count) + ')'
+	page.should have_content type ' (' + number_with_delimiter(count) + ')'
 end	
 
 When(/^I navigate to Quick Purchase$/) do
@@ -39,4 +39,73 @@ end
 
 Then(/^I see a subtotal of USD (.*)$/) do |estimatedTotal|
 	find(:css, '#estimatedBasketTotal1').should have_content(estimatedTotal)
+end
+
+
+When(/^I open the Mega\-Nav "(.*?)" menu$/) do |topmenu|
+	#find(:xpath, "//a[text()='"+topmenu+"']").click
+	find(:xpath, "//a[text()='" + topmenu + "']").hover
+end
+
+When(/^I select "(.*?)" sub\-menu$/) do |sub_menu|
+  find(:xpath, "//a[text()='Biopharmaceutical']").hover
+end
+
+When(/^I select "(.*?)" sub\-sub\-menu$/) do |sub_sub_menu|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I get a CMS page containing text "(.*?)"$/) do |cms_text|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I get a CMS page containing left menu "(.*?)"$/) do |left_menu|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I get a top left CMS area containing "(.*?)"$/) do |left_cms_area|
+  pending # express the regexp above with the code you wish you had
+end
+
+
+
+
+When(/^I navigate to the register page$/) do
+  visit ui_url 'ViewUserAccount-ShowRegister'
+end
+
+When(/^I select Full Registration$/) do
+	find(:xpath, "//a[text()='Full Registration']").click
+end
+
+When(/^I fill\-in these fields at Register page:$/) do |table|
+	table.hashes.map do |hash|  @table = table.raw
+		if hash["Field"] == 'MerckFullRegistration_Title'
+			select(hash["Value"], :from => 'MerckFullRegistration_Title')
+		else
+			fill_in(hash["Field"], :with => hash["Value"])  
+		end
+	end
+	check('MerckFullRegistration_TermAndCondition')
+	find(:xpath, "//button[@name='login']").click
+end
+
+When(/^I fill\-in these fields at Address page:$/) do |table|
+	table.hashes.map do |hash|  @table = table.raw
+		if hash["Field"] == 'MerckAddressFormWithState_ShippingCountryCode' then
+			select(hash["Value"], :from => 'MerckAddressFormWithState_ShippingCountryCode')
+			
+		elsif hash["Field"] == 'MerckAddressFormWithState_ShippingState' then
+			select(hash["Value"], :from => 'MerckAddressFormWithState_ShippingState')
+			
+		else
+			fill_in(hash["Field"], :with => hash["Value"])  
+		end
+	end
+	find(:xpath, "//button[@id='submitAddress']").click
+end
+
+
+Then(/^I get a page reading "(.*?)" and "(.*?)"$/) do |str1, str2|
+  page.should have_content str1 and page.should have_content str2 
 end
